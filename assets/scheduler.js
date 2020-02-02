59309deb1d8ff0e8a9cf54bb2b953db92a4d3fb8
Brendan Gallagher <bpgallagher27@gmail.com>
2020-02-02 18:59:18 -0500

    first timebox functionality

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -2,29 +2,27 @@ $(document).ready(function() {
 
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-    //function to call the info from local storage
-    //storedEvents9();
-
-    //let am9 = $("#9am").val();
+    let na = "Brendan";
+    localStorage.setItem("name", JSON.stringify(na));
+    let myName = JSON.parse(localStorage.getItem("name"));
+    console.log(myName);
     
     $("#save9").on("click" , function() {
         let am9 = $("#9am").val();
         localStorage.setItem("9oClock" , JSON.stringify(am9));
-        let myWork = JSON.parse(localStorage.getItem("9oClock"));
-        console.log(myWork);
     });
+    
+    let myWork9 = JSON.parse(localStorage.getItem("9oClock"));
+    console.log(myWork9);
+    $("#9am").val(myWork9);
 
-    let na = "Brendan";
+    $("#save10").on("click" , function() {
+        let am9 = $("#10am").val();
+        localStorage.setItem("10oClock" , JSON.stringify(am9));
+    });
+    
+    let myWork10 = JSON.parse(localStorage.getItem("10oClock"));
+    console.log(myWork10);
+    $("#10am").val(myWork10);
 
-    localStorage.setItem("name", JSON.stringify(na));
-
-
-    let myName = JSON.parse(localStorage.getItem("name"));
-
-    console.log(myName);
-
-    //function storedEvents9() {
-        //let post9 = localStorage.getItem("9oclock");
-        //$("#9am").text(post9);
-    }
-);
+});
