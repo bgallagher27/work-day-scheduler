e9fb36b6903620904bcdeb7e2d5e16e38d76269d
Brendan Gallagher <bpgallagher27@gmail.com>
2020-02-02 15:13:24 -0500

    update

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
 
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
+    //function to call the info from local storage
+    storedEvents9();
 
+    let am9 = $("#9am").val();
     
+    $("#save9").on("click" , function() {
+        localStorage.setItem("9oClock" , am9);
+    });
+
+    function storedEvents9() {
+        let post9 = localStorage.getItem("9oclock");
+        $("#9am").text(post9);
+    }
 });
