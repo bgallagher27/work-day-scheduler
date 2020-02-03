8f865f65899390ffa3a360f82de46edc1aeaa8de
Brendan Gallagher <bpgallagher27@gmail.com>
2020-02-02 19:17:29 -0500

    11 timebox functionality

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
 
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-
-    let na = "Brendan";
-    localStorage.setItem("name", JSON.stringify(na));
-    let myName = JSON.parse(localStorage.getItem("name"));
-    console.log(myName);
     
+    // This is the 9 am timebox
     $("#save9").on("click" , function() {
         let am9 = $("#9am").val();
         localStorage.setItem("9oClock" , JSON.stringify(am9));
@@ -16,13 +12,24 @@ $(document).ready(function() {
     console.log(myWork9);
     $("#9am").val(myWork9);
 
+    // This is the 10 am timebox
     $("#save10").on("click" , function() {
-        let am9 = $("#10am").val();
-        localStorage.setItem("10oClock" , JSON.stringify(am9));
+        let am10 = $("#10am").val();
+        localStorage.setItem("10oClock" , JSON.stringify(am10));
     });
     
     let myWork10 = JSON.parse(localStorage.getItem("10oClock"));
     console.log(myWork10);
     $("#10am").val(myWork10);
+
+    // This is the 11 am timebox
+    $("#save11").on("click" , function() {
+        let am11 = $("#11am").val();
+        localStorage.setItem("11oClock" , JSON.stringify(am11));
+    });
+
+    let myWork11 = JSON.parse(localStorage.getItem("11oClock"));
+    console.log(myWork11);
+    $("#11am").val(myWork11);
 
 });
